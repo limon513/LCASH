1a8deb8d354da5e428a1485f7cdb5084fff45bcd
limon <sabur.islam@sec.ac.bd>
2024-08-12 11:11:27 +0600

    FEAT: Made transfer api idempotent with local storage.(simultaing redis and stuff)

--- src/controllers/transfer-controller.js
+++ src/controllers/transfer-controller.js
@@ -1,22 +1,44 @@
 const serverConfig = require('../config/server-config');
 const TransferService = require('../services/transfer-service');
-const {ErrorResponse,SuccessResponse} = require('../utils/common');
-const {StatusCodes} = require('http-status-codes');
+const { ErrorResponse, SuccessResponse } = require('../utils/common');
+const { StatusCodes } = require('http-status-codes');
 const AppError = require('../utils/errors/app-error');
 
-async function CashOut(req,res){
+const IdempotencyRedis = {};
+
+async function CashOut(req, res) {
     req.body.senderAccount = req.body.accNumber;
     try {
-        const response = await TransferService.CashOut(req.body);
-        SuccessResponse.data = response;
-        return res.status(StatusCodes.ACCEPTED).json(SuccessResponse);
-    } catch (error) {
-        if(error instanceof Error){
-            ErrorResponse.error = error;
-            return res.status(ErrorResponse.error.statusCode).json(ErrorResponse);
+        const idempotencyKey = req.headers['x-idempotency-key'];
+
+        if (!idempotencyKey) {
+            throw new AppError(['Idempotency Key missing!'], StatusCodes.BAD_REQUEST);
         }
-        ErrorResponse.error = new AppError(['Service unavailale!'],StatusCodes.INTERNAL_SERVER_ERROR);
-        return res.status(ErrorResponse.error.statusCode).json(ErrorResponse);
+
+        if (IdempotencyRedis[idempotencyKey]) {
+            throw new AppError(['Duplicate Request!'], StatusCodes.BAD_REQUEST);
+        }
+
+        const response = await TransferService.CashOut(req.body);
+
+        IdempotencyRedis[idempotencyKey] = idempotencyKey;
+
+        SuccessResponse.data = response;
+        return res
+            .status(StatusCodes.ACCEPTED)
+            .json(SuccessResponse);
+
+    } catch (error) {
+        if (error instanceof Error) {
+            ErrorResponse.error = error;
+            return res
+                        .status(ErrorResponse.error.statusCode)
+                        .json(ErrorResponse);
+        }
+        ErrorResponse.error = new AppError(['Service unavailale!'], StatusCodes.INTERNAL_SERVER_ERROR);
+        return res
+                    .status(ErrorResponse.error.statusCode)
+                    .json(ErrorResponse);
     }
 }
 
