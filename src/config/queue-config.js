709af3b2c480b616a0c929384ae108f40357a1df
limon <sabur.islam@sec.ac.bd>
2024-08-17 13:05:04 +0600

    ISSUE FIX #4 Notification and Messege Queue microservice is up.

--- src/config/queue-config.js
+++ src/config/queue-config.js
@@ -1,12 +1,13 @@
 const amqplib = require('amqplib');
 const Logger  = require('./logger-config');
 
+let channel,connection;
+
 async function connectQueue(){
     try {
-        const connection = await amqplib.connect('amqp://localhost');
-        const channel = await connection.createChannel();
+        connection = await amqplib.connect('amqp://localhost');
+        channel = await connection.createChannel();
         await channel.assertQueue('Lcash-Noti');
-        return channel;
     } catch (error) {
         Logger.log({
             level:'info',
@@ -17,6 +18,15 @@ async function connectQueue(){
     }
 }
 
+async function sendData(data){
+    try {
+        await channel.sendToQueue('Lcash-Noti',Buffer.from(JSON.stringify(data)));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
     connectQueue,
+    sendData,
 }
--- src/utils/crons/cron-jobs.js
+++ src/utils/crons/cron-jobs.js
@@ -3,59 +3,98 @@ const cron = require('node-cron');
 const { RequestService, TransferService, EmailService } = require('../../services');
 const { Enums, Utility } = require('../common');
 const serverConfig = require('../../config/server-config');
+const { QUEUE } = require('../../config');
 
-function scheduleCrons(){
+function scheduleCrons() {
     cron.schedule('00 00 * * *', async () => {
         const response = await RequestService.resetTransferLimit();
-        if(response){
+        if (response) {
             Logger.log({
-                level:'info',
-                message:'Limit Reset Successfull',
-                label:__filename,
-                errors:{}
+                level: 'info',
+                message: 'Limit Reset Successfull',
+                label: __filename,
+                errors: {}
             });
         }
         else {
             Logger.log({
-                level:'info',
-                message:'Limit Reset Failed',
-                label:__filename,
-                errors:{}
+                level: 'info',
+                message: 'Limit Reset Failed',
+                label: __filename,
+                errors: {}
             });
         }
     });
 
-    cron.schedule('*/3 * * * *', async () => {
+    cron.schedule('*/1 * * * *', async () => {
         try {
             const transfers = await TransferService.getPendingNotifiedTransactions(Enums.NOTIFIED_STATUS.PENDING);
-            console.log(transfers);
-            transfers.forEach(async (transfer)=>{
+            transfers.forEach(async (transfer) => {
                 const message = Utility.createResponseForTransfer(transfer.status,
                     transfer.transactionId,
                     transfer.transactionType,
                     transfer.amount,
                     transfer.charges,
                     transfer.reciverAccount);
-                if(transfer.senderEmail){
-                    await EmailService.sendEmail(serverConfig.GMAILMAIL,
-                        transfer.senderEmail,
-                        'Transaction Info',
-                        message);
+                if (transfer.senderEmail) {
+                    const data = {
+                        message:message,
+                        senderEmail:transfer.senderEmail,
+                    };
+                    await QUEUE.connectQueue();
+                    await QUEUE.sendData(data);
                 }
 
-                if(transfer.status === Enums.TRANSACTION_STATUS.SUCCESSFUL && transfer.reciverEmail){
-                    await EmailService.sendEmail(serverConfig.GMAILMAIL,
-                        transfer.reciverEmail,
-                        'Transaction Info',
-                        Utility.createResponseForDeposite(transfer.transactionId,transfer.amount,transfer.senderAccount));
+                if (transfer.status === Enums.TRANSACTION_STATUS.SUCCESSFUL && transfer.reciverEmail) {
+                    const data = {
+                        message: Utility.createResponseForDeposite(transfer.transactionId,transfer.amount,transfer.senderAccount),
+                        reciverEmail:transfer.reciverEmail,
+                    }
+                    await QUEUE.connectQueue();
+                    await QUEUE.sendData(data);
                 }
 
                 await TransferService.resolveTransferOnNotification(transfer.transactionId);
             });
-        } catch (error) {   
+        } catch (error) {
             console.log(error);
         }
     });
+
+    // cron.schedule('*/3 * * * *', async () => {
+    //     try {
+    //         const transfers = await TransferService.getPendingNotifiedTransactions(Enums.NOTIFIED_STATUS.PENDING);
+    //         console.log(transfers);
+    //         transfers.forEach(async (transfer)=>{
+    //             const message = Utility.createResponseForTransfer(transfer.status,
+    //                 transfer.transactionId,
+    //                 transfer.transactionType,
+    //                 transfer.amount,
+    //                 transfer.charges,
+    //                 transfer.reciverAccount);
+    //             if(transfer.senderEmail){
+    //                 await EmailService.sendEmail(serverConfig.GMAILMAIL,
+    //                     transfer.senderEmail,
+    //                     'Transaction Info',
+    //                     message);
+    //             }
+
+    //             if(transfer.status === Enums.TRANSACTION_STATUS.SUCCESSFUL && transfer.reciverEmail){
+    //                 await EmailService.sendEmail(serverConfig.GMAILMAIL,
+    //                     transfer.reciverEmail,
+    //                     'Transaction Info',
+    //                     Utility.createResponseForDeposite(transfer.transactionId,transfer.amount,transfer.senderAccount));
+    //             }
+
+    //             await TransferService.resolveTransferOnNotification(transfer.transactionId);
+    //         });
+    //     } catch (error) {   
+    //         console.log(error);
+    //     }
+    // });
+
+
+
 }
 
 module.exports = {
